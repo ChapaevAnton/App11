c1352537f6ab60ab9003787f458c1c812a676da3
Chapaev Anton <virsus2009@gmail.com>
2021-03-07 14:02:46 +0700

    up to 11.3.5

--- src/shipp/Main.java
+++ src/shipp/Main.java
@@ -1,5 +1,9 @@
 package shipp;
 
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     // TODO: 07.03.2021 11.3.2
@@ -11,6 +15,19 @@ public class Main {
         shippableBox.ship(box);
         shippableBox.ship(new Box<Number>(1));
 
+        Collection<? extends String> collection = new LinkedList<>(); // 2
+        collection.add("Hello"); // 3
+        System.out.println(collection.size());
+
 
     }
+
+    private static void modifyList(List<? extends Number> list) {
+
+        list.get(0).doubleValue();
+        list.add(5.6d); // does not compile
+        list.add(131232134342344L); // does not compile
+        list.add(118); // does not compile
+    }
+
 }
